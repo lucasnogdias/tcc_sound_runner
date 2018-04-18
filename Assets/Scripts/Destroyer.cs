03cb4dc8864f9bc6aad13742de32ea91a32a50a1
lucasnogdias <lucas_ndias@hotmail.com>
2018-04-18 19:28:39 -0300

    Object destruction on colission and game reset

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Destroy(coll.gameObject);
+    }
+}
