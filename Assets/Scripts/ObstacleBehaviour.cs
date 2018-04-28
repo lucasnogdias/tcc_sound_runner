d0993639c94be04caa8877ddca8a60499ae48b8b
lucasnogdias <lucas_ndias@hotmail.com>
2018-04-28 18:33:27 -0300

    Randomized pitch for easier sound source identification

---
+++ Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour {
+
+    public AudioSource obstacleAudio;
+
+	// Use this for initialization
+	void Start () {
+        obstacleAudio.pitch = Random.Range(0.3f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
