17fb196c45949f84948eff105269fcf7464a4da5
lucasnogdias <lucas_ndias@hotmail.com>
2018-08-04 00:56:59 -0300

    Start and End Screens, player lives and game over and basic scoing

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameObject instance = null;
+
+    public int playerScore = 0;
+    public int scoreMultiplier = 0;
+    public int currentLevel = 0;
+    public int pointsToNextLevel = 100;
+    public int pointStreak = 0;
+    public int basePointValue = 5;
+
+    // Use this for initialization
+    void Start() {
+        if (GameController.instance == null)
+        {
+            GameController.instance = this.gameObject;
+        }
+
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void scorePoints(){
+        this.playerScore += this.basePointValue * this.scoreMultiplier;
+        this.pointStreak++;
+        if (this.playerScore > this.pointsToNextLevel)
+        {
+            this.currentLevel += 1;
+        }
+        if (this.pointStreak%10 == 0 && scoreMultiplier<10)
+        {
+            this.scoreMultiplier++;
+        }
+    }
+
+}
